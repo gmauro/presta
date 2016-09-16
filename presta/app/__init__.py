29db81397ac7a5d8f0d91a7e69c582681861425c
ratzeni <rossano.atzeni@crs4.it>
2016-09-16 14:15:01 +0200

    Periodic Tasks

--- presta/app/__init__.py
+++ presta/app/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 app = Celery('app',
              include=['app.tasks'])
@@ -9,6 +10,18 @@ app = Celery('app',
 os.environ.setdefault('CELERY_CONFIG_MODULE', 'app.celeryconfig')
 
 app.config_from_envvar('CELERY_CONFIG_MODULE')
+app.conf.update(
+    CELERY_ENCELERYBEAT_SCHEDULE={
+
+        # Execute every three hours: midnight, 3am, 6am, 9am, noon, 3pm, 6pm, 9pm.
+
+        'check-every-three-minutes': {
+            'task': 'presta.app.tasks.check_rd_ready_to_be_preprocessed',
+            'schedule': crontab(minute='*/1'),
+            'args': 'SEQ_RUNNING',
+        },
+    }
+)
 
 if __name__ == '__main__':
     app.start()
