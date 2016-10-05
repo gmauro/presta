41ddcfc7bf0d0939abc97b1bf58c5d87498d37a3
ratzeni <rossano.atzeni@crs4.it>
2016-10-05 11:46:03 +0200

    Moving cron_config on presta_dep

--- presta/app/__init__.py
+++ presta/app/__init__.py
@@ -9,17 +9,6 @@ app = Celery('app',
 os.environ.setdefault('CELERY_CONFIG_MODULE', 'app.celeryconfig')
 
 app.config_from_envvar('CELERY_CONFIG_MODULE')
-# app.conf.update(
-#     CELERYBEAT_SCHEDULE={
-#         # Execute every three hours: midnight, 3am, 6am, 9am, noon, 3pm, 6pm, 9pm.
-#         'check_rd_ready_to_be_preprocessed': {
-#             'task': 'presta.app.tasks.check_rd_ready_to_be_preprocessed',
-#             #'schedule': crontab(minute=0, hour='*/3'),
-#             'schedule': crontab(minute='*/1'),
-#             'kwargs': dict(),
-#         },
-#     }
-# )
 
 if __name__ == '__main__':
     app.start()
