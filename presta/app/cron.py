f447f29996b3474aff000455bb55fea302b22f4b
ratzeni <rossano.atzeni@crs4.it>
2016-10-19 14:52:38 +0200

    Developing events queue - using dict of functions

--- presta/app/cron.py
+++ presta/app/cron.py
@@ -10,5 +10,5 @@ logger = get_task_logger(__name__)
 @app.task(name='presta.app.cron.check_rd_ready_to_be_preprocessed')
 def check_rd_ready_to_be_preprocessed():
     logger.info('Cron Task: searching for run ready to be preprocessed...')
-    emit_event(event='check_rd', params=dict()).delay()
+    emit_event.si(event='check_rd', params=dict()).delay()
     return True
