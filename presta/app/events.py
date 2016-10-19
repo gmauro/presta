ee865dec390730c52cc062ff043ddcd6f18b57fb
ratzeni <rossano.atzeni@crs4.it>
2016-10-19 12:14:59 +0200

    Developing events queue - using dict of functions

--- presta/app/events.py
+++ presta/app/events.py
@@ -6,22 +6,21 @@ from presta.utils import runJob
 
 logger = get_task_logger(__name__)
 
-
 tasks = {}
 task = lambda f: tasks.setdefault(f.__name__, f)
 
 
 @task
-def check_rd(**kwargs):
+def check_rd(params=dict()):
     cmd_line = ['presta', 'check', '--emit_events']
     output = runJob(cmd_line, logger)
     return True if output else False
 
 
 @task
-def rd_ready(**kwargs):
-    rd_path = kwargs.get('rd_path')
-    rd_label = kwargs.get('rd_label')
+def rd_ready(params=dict()):
+    rd_path = params.get('rd_path')
+    rd_label = params.get('rd_label')
     logger.info('{} is ready to be processed. Start preprocessing...'.format(rd_label))
     cmd_line = ['presta', 'proc', '--rd_path', rd_path, '--export_qc']
     output = runJob(cmd_line, logger)
@@ -29,5 +28,5 @@ def rd_ready(**kwargs):
 
 
 @app.task(name='presta.app.events.emit_event')
-def emit_event(event=None, **kwargs):
-    tasks[event](kwargs)
+def emit_event(event=None, params=dict()):
+    tasks[event](params)
