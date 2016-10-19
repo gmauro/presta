d1d021d1e85fe90b650a21b904248f49c797496e
ratzeni <rossano.atzeni@crs4.it>
2016-10-19 11:52:32 +0200

    Developing events queue - using dict of functions

--- presta/app/cron.py
+++ presta/app/cron.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
 
 from . import app
-from celery.utils.log import get_task_logger
-
-logger = get_task_logger(__name__)
-from presta.utils import runJob
 from presta.app.events import emit_event
+
+from celery.utils.log import get_task_logger
+logger = get_task_logger(__name__)
+
 
 
 @app.task(name='presta.app.cron.check_rd_ready_to_be_preprocessed')
