1a0cac3f80465c0dae4e13d8c324cad8d384ee93
Gianmauro Cuccuru <gmauro@crs4.it>
2016-11-08 11:46:35 +0100

    pinned celery version

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ AUTHOR_INFO = [
   ]
 MAINTAINER_INFO = [
   ("Gianmauro Cuccuru", "gianmauro.cuccuru@crs4.it"),
+  ("Rossano Atzeni", "rossano.atzeni@crs4.it"),
   ]
 AUTHOR = ", ".join(t[0] for t in AUTHOR_INFO)
 AUTHOR_EMAIL = ", ".join("<%s>" % t[1] for t in AUTHOR_INFO)
@@ -13,13 +14,13 @@ MAINTAINER = ", ".join(t[0] for t in MAINTAINER_INFO)
 MAINTAINER_EMAIL = ", ".join("<%s>" % t[1] for t in MAINTAINER_INFO)
 
 setup(name="presta",
-      version='0.3',
+      version='0.4',
       description="Utility to process sequencing data",
       author=AUTHOR,
       author_email=AUTHOR_EMAIL,
       maintainer=MAINTAINER,
       maintainer_email=MAINTAINER_EMAIL,
-      install_requires=['alta', 'celery', 'drmaa', 'ansible>=2'],
+      install_requires=['alta', 'celery==4.0.0', 'drmaa', 'ansible>=2'],
       scripts=glob.glob('scripts/*'),
       packages=find_packages(),
       include_package_data=True,
@@ -29,7 +30,7 @@ setup(name="presta",
       ],
       license='MIT',
       platforms="Posix; MacOS X; Windows",
-      classifiers=["Development Status :: 3 - Alpha",
+      classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
                    "License :: OSI Approved :: MIT License",
                    "Operating System :: OS Independent",
