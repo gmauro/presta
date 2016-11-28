0cd35578bdd5a6a0b6e9ecf75330f60c3e9d5429
ratzeni <rossano.atzeni@crs4.it>
2016-11-28 16:31:07 +0100

    Delivery - Fixed Bugs on merge

--- presta/app/tasks.py
+++ presta/app/tasks.py
@@ -239,7 +239,7 @@ def copy(src, dest):
 
 
 @app.task(name='presta.app.tasks.remove')
-def remove(files=list(),  **kwargs):
+def remove(files=list()):
     result = False
     try:
         for f in files:
